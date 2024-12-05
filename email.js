3c59a6c5a19595125c83064b713fadc4dfb8c1d1
Agus <agustinguevara1949@gmail.com>
2024-12-04 23:31:33 -0300

    resolucion

--- email.js
+++ email.js
@@ -1,24 +1,20 @@
-// Incluye la librería de EmailJS
 (function() {
-    emailjs.init("RDl8sF7yI6nHxx5GP"); // Sustituye "TU_USER_ID" por tu User ID de EmailJS
+    emailjs.init("RDl8sF7yI6nHxx5GP");
 })();
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar el envío por defecto del formulario
+    event.preventDefault(); 
 
-    // Obtiene los valores del formulario
     const userName = document.getElementById("user-name").value;
     const userEmail = document.getElementById("user-email").value;
     const userMessage = document.getElementById("user-message").value;
 
-    // Datos para EmailJS
     const templateParams = {
         from_name: userName,
         email_id: userEmail,
         message: userMessage,
     };
 
-    // Envía el correo con EmailJS
     emailjs.send("default_service", "template_yzydmef", templateParams)
         .then(function(response) {
             console.log("SUCCESS!", response.status, response.text);
